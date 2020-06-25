899a6ce55eee653be29fb1e47df84fc39aea290f
eliataylor <eli@taylormadetraffic.com>
2020-06-24 17:45:04 -0700

    working copy with some instructions

--- src/App.js
+++ src/App.js
@@ -1,66 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import TestFetch from './TestFetch';
-
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {subReddit: "", tickers: {}};
+    this.state = {subReddit: "pennystocks", topJson:[], subJson:[], tickers: {}};
     // This binding is necessary to make `this` work in the callback
     this.start = this.start.bind(this);
   }
 
-  start(subreddit) {
-    let url = "https://www.reddit.com/r/"+subreddit+".json";
-    let json = this.loadJSON(url);
-
-    //pasrse -> sentence to object
-
-    for(var i=0; i < json.data.children; i++) {
-      this.parseForTickets(json.data.children[i]);
-      let commentURL = "https://www.reddit.com/r/pennystocks/comments/"+json.data.children[i].id+".json";;
-      let jsonComments = this.loadJSON(commentURL);
-      this.parseForTickers(jsonComments);
-    }
-
-
-  }
-
-
-  loadJSON(url) {
-    console.log("Works");
+  loadJSON(url, callback) {
     fetch(url)
-      .then(res => res.json())
-      .then(
-
-        (result) => {
-          console.log(result);
-          this.setState({
-            isLoaded: true,
-            items: result.data.children
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          console.log(error);
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
+        .then(res => res.json())
+        .then(result => {
+              console.log(result);
+              callback(result);
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+              console.log(error);
+              this.setState({
+                isLoaded: true,
+                error
+              });
+            }
+        )
   }
 
+  start() {
+    if (this.state.subReddit.length === 0) {
+      return alert('please enter a subreddit');
+    }
+    let url = "https://www.reddit.com/r/"+this.state.subReddit+".json";
+    const that = this; // since loadJSON takes an 'anyonymous' function, it cannot use .bind
+    this.loadJSON(url, function(result) {
+      that.setState({topJson: result.data.children});
+      result.data.children.map( (item, i) => {
+        if (item.kind === 't3') {
+          that.parseForTickers(item.data);
+        } else {
+          that.parseForTickers(item.data);
+        }
+        let commentURL = "https://www.reddit.com/r/pennystocks/comments/"+item.data.id+".json";
+        console.log("NEXT UP  " + commentURL);
+        /** jahi  THIS NEXT LINE WILL NOT WORK.
+         * you can look at the console under "NEXT UP" to see the structure of comments
+         * you'll need to write a new function to pass as the callback to loadJSON below
+         * that callback should receive a comment object, so you'll need to parse the results of loadJSON a little deeper.
+        **/
+        that.loadJSON(commentURL, that.parseForTickers);
+        return true;
+      });
+    });
+  }
 
   //parsing -> traversing parts of a data structure to access a specific value point;
 
-  parseForTickets(jsonComments) {
-    let text = jsonComments.title || jsonComments.selftext || jsonComments.body;
+  parseForTickers(jsonComments) {
+    console.log(jsonComments);
+    let text = jsonComments.selftext || jsonComments.body || jsonComments.title;
+    if (!text) {
+      return console.log('NO TEXT PROPERTY FOUND', jsonComments);
+    }
+    console.log("parsing ", text);
     let matches = text.match(/[A-Z]{3}/g); // weak regex for stock tickers
     // matches = ["HFS", "GPX", "AJS"];
 
@@ -77,26 +82,33 @@ class App extends React.Component {
 
     this.setState({tickers: tickers});
 
-    if (jsonComments.num_comments > 0) {
-      var nextComments = {};
-      this.parseForTickets(nextComments);
+    var nextComments = {};
+    if (jsonComments.num_comments && jsonComments.num_comments.length > 0) {
+      if (jsonComments.num_comments > 0) {
+        this.parseForTickers(nextComments);
+      }
     }
 
-    if (jsonComments.replies.length > 0) {
-      var nextComments = {};
-      this.parseForTickets(nextComments);
+    if (jsonComments.replies && jsonComments.replies.length > 0) {
+      nextComments = {};
+      this.parseForTickers(nextComments);
     }
   }
 
-  render()
-  {
+  render()  {
+    var tickerLis = [];
+    for(var code in this.state.tickers) {
+      tickerLis.push(<li key={code}> {code} = {this.state.tickers[code]}</li>)
+    }
+
     return (
       <div>
-        <form>
-          <input type="text" className="reddit-search" name="reddit-name" placeholder="enter reddit url" />
+          <input type="text" onChange={e => this.setState({subReddit: e.target.value})}
+                 value={this.state.subReddit} placeholder="enter reddit url" />
           <button className="searc-btn" onClick={this.start}>Search</button>
-        </form>
-        <testFetch />
+        <ul>
+          {tickerLis}
+        </ul>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import TestFetch from './TestFetch';
+// import TestFetch from './TestFetch';
 
 ReactDOM.render(
   <React.StrictMode>
